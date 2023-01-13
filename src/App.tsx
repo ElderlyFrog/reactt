e09ce0df812427cdb0f619bfbb2da9a77b2b4c83
ElderlyFrog <113665414+ElderlyFrog@users.noreply.github.com>
2023-01-13 10:19:33 +0100

    TEST

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import type { ReactNode } from "react"
+import {produkty} from "./produkty";
 
+const data = produkty;
+
+
+interface dataPropsTypes {
+  name:string, price:string,  category:string,  brand:string,  description:string
+
+}
+const Leti =(props: dataPropsTypes) => {
+  return  (
+      <div>
+        <h1>{props.name}</h1>
+        <h3>{props.price}</h3>
+        <h3>{props.category}</h3>
+        <h3>{props.brand}</h3>
+        <h3>{props.description}</h3>
+
+
+      </div>
+  )
+}
+
+
+interface ObalPropsTypes {
+  children: ReactNode
+}
+function Obal({children }: ObalPropsTypes   ) {
+  return (
+      <div >
+        {children}
+
+
+
+      </div>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+      <Obal>
+
+          {data.map((leti:{name:string, price:string,  category:string,  brand:string,  description:string }) => <Leti{...leti}/>)}
+
+      </Obal>
+        </>
+  )
 }
 
 export default App;
